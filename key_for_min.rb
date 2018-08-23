6522debf5b650f88183fa46a78f867733d811853
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-23 21:45:08 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
     if name_hash.empty?
     nil
   else
-    min_key = ""
-    min_value = 0
+    min_key = name_hash.first[0]
+    min_value = name_hash.first[1]
     name_hash.each do |key, value|
-      if min_value == 0 || value < min_value
+      if value < min_value
         min_value = value
         min_key = key
       end
