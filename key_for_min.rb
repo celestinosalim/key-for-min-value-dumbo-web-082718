e504e1bf59ce775ecbb22b50ea940427f88673ab
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-23 21:41:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-    if name_hash.empty?
-      nil
+    # if name_hash.empty?
+    #   nil
+    if name_hash == {}
+    return nil
     else
       firstKey, firstVal = name_hash.first
     name_hash.each do |key, value|
       if firstVal > value
         firstVal = value
-        firstKey = key 
+        firstKey = key
          end
        end
   end
