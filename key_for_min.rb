6f17453f4218c176928bbc25ed688ca1568bb66d
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-23 21:51:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,16 +7,11 @@ def key_for_min_value(name_hash)
   else
     min_key = name_hash.first[0]
     min_value = name_hash.first[1]
-    # name_hash.each do |key, value|
-      # if value < min_value
-      #   min_value = value
-      #   min_key = key
-      # end
-
-      while value < min_value
-        value  =  min_value
-        key = min_key
-
+    name_hash.each do |key, value|
+      if value < min_value
+        min_value = value
+        min_key = key
+      end
     end
       min_key
     end
